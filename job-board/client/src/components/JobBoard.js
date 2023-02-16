626aa00acc5a9bd6a9176d02c196d6d90ae5225a
shamim-5 <shamim.somadder5@gmail.com>
2023-02-16 19:51:00 +0600

    use graphql-request and fetch data from react client

--- job-board/client/src/components/JobBoard.js
+++ job-board/client/src/components/JobBoard.js
@@ -1,7 +1,16 @@
 import JobList from './JobList';
-import { jobs } from '../fake-data';
+// import { jobs } from '../fake-data';
+import { getJobs } from '../graphql/queries';
+import { useEffect, useState } from 'react';
 
 function JobBoard() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    getJobs().then(setJobs)
+  }, [])
+  
+  console.log('[JobBoard] jobs:', jobs);
   return (
     <div>
       <h1 className="title">
---
+++ job-board/client/src/graphql/queries.js
@@ -0,0 +1,20 @@
+import request, { gql } from "graphql-request";
+
+const GRAPHQL_URL = "http://localhost:9000/graphql";
+
+export async function getJobs() {
+  const query = gql`
+    query {
+      jobs {
+        id
+        title
+        company {
+          name
+        }
+      }
+    }
+  `;
+
+  const { jobs } = await request(GRAPHQL_URL, query);
+  return jobs;
+}
